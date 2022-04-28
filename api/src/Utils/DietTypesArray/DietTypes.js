559de710cee117e11fadf82320f1996600a8c945
Paola <paola.benavides35@gmail.com>
2022-04-28 13:38:31 -0500

    All diet types

---
+++ api/src/Utils/DietTypesArray/DietTypes.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dietTypes: [
+    "gluten free",
+    "ketogenic",
+    "vegetarian",
+    "lacto ovo vegetarian",
+    "vegan",
+    "pescatarian",
+    "paleolithic",
+    "primal",
+    "whole30",
+  ],
+};
