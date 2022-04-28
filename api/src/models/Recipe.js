6a05c7c7cb9d85b0ecdf8e939f83f346379a031d
Paola <paola.benavides35@gmail.com>
2022-04-28 11:35:37 -0500

    Finish models

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,12 +1,39 @@
-const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+const {DataTypes} = require("sequelize");
+
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('recipe', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "Recipe",
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notNull: {msg: "The recipe must be completed"},
+        },
+      },
+      summary: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {notNull: {msg: "The summary must be completed"}},
+      },
+      spoonacularScore: {
+        type: DataTypes.INTEGER,
+      },
+      healthScore: {type: DataTypes.INTEGER},
+      analyzedInstructions: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+      },
+      image: {type: DataTypes.TEXT},
+      id: {
+        allowNull: false,
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      diets: {type: DataTypes.ARRAY(DataTypes.STRING)},
+      dishTypes: {type: DataTypes.ARRAY(DataTypes.STRING)},
     },
-  });
+    {timestamps: false}
+  );
 };
