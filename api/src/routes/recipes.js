da11409230e4418513c5ccfc7f80267b30579f28
Paola <paola.benavides35@gmail.com>
2022-04-28 13:56:18 -0500

    Finish routes

---
+++ api/src/routes/recipes.js
@@ -0,0 +1,14 @@
+const {Router} = require("express");
+const {
+  getRecipesbyName,
+  getRecipesbyId,
+  postRecipe,
+} = require("../Functions/recipesFunctions");
+
+const router = Router();
+
+router.get("/", getRecipesbyName);
+router.get("/:id", getRecipesbyId);
+router.post("/", postRecipe);
+
+module.exports = router;
---
+++ api/src/routes/types.js
@@ -0,0 +1,7 @@
+const {Router} = require("express");
+const {getTypes} = require("../Functions/typesFunctions.js");
+const router = Router();
+
+router.get("/", getTypes);
+
+module.exports = router;
