7f1e6d44d22aa3cc94999ef9e24690bc49ad4be0
Paola <paola.benavides35@gmail.com>
2022-04-28 19:55:09 -0500

    added details

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,39 +1,35 @@
 const {DataTypes} = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define(
-    "Recipe",
-    {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          notNull: {msg: "The recipe must be completed"},
-        },
+  sequelize.define("Recipe", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notNull: {msg: "The recipe must be completed"},
       },
-      summary: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {notNull: {msg: "The summary must be completed"}},
-      },
-      spoonacularScore: {
-        type: DataTypes.INTEGER,
-      },
-      healthScore: {type: DataTypes.INTEGER},
-      analyzedInstructions: {
-        type: DataTypes.ARRAY(DataTypes.STRING),
-      },
-      image: {type: DataTypes.TEXT},
-      id: {
-        allowNull: false,
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue: DataTypes.UUIDV4,
-      },
-      diets: {type: DataTypes.ARRAY(DataTypes.STRING)},
-      dishTypes: {type: DataTypes.ARRAY(DataTypes.STRING)},
     },
-    {timestamps: false}
-  );
+    summary: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {notNull: {msg: "The summary must be completed"}},
+    },
+    spoonacularScore: {
+      type: DataTypes.INTEGER,
+    },
+    healthScore: {type: DataTypes.INTEGER},
+    analyzedInstructions: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+    },
+    image: {type: DataTypes.TEXT},
+    id: {
+      allowNull: false,
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    diets: {type: DataTypes.ARRAY(DataTypes.STRING)},
+    dishTypes: {type: DataTypes.ARRAY(DataTypes.STRING)},
+  });
 };
