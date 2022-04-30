93a1e92ef6957ad74fd2b18972d28cfa92bdb11e
Paola <paola.benavides35@gmail.com>
2022-04-30 15:20:39 -0500

    created components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import "./App.css";
+import {Route} from "react-router";
+import Landing from "./Components/Landing/Landing.js";
+import Home from "./Components/Home/Home.js";
+import Details from "./Components/Details/Details.js";
+import CreateRecipe from "./Components/CreateRecipe/CreateRecipe.js";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Food</h1>
+    <div>
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/details/:id" component={Details} />
+      <Route exact path="/createrecipe" component={CreateRecipe} />
     </div>
   );
 }
